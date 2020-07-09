3fd1c88d156cefd1c1c41ec64ba24f143e71c04f
keerthanavelan <67827550+keerthanavelan@users.noreply.github.com>
2020-07-09 18:03:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+}
+
+function draw() {
+  background(0,0,0);  
+  
+  if (isTouching(fixedRect,movingRect)){
+    movingRect.shapeColor = "plum";
+    fixedRect.shapeColor = "plum";
+  }
+  else {
+    movingRect.shapeColor = "blue";
+    fixedRect.shapeColor = "red";
+  }
+  // if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+  //     && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2) {
+  //   movingRect.velocityX = movingRect.velocityX * (-1);
+  //   fixedRect.velocityX = fixedRect.velocityX * (-1);
+  // }
+  // if (movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+  //   && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
+  //   movingRect.velocityY = movingRect.velocityY * (-1);
+  //   fixedRect.velocityY = fixedRect.velocityY * (-1);
+  // }
+  drawSprites();
+}
+
+function isTouching(object1,object2) {
+  if  (object1.x-object2.x < object1.width/2 + object2.width/2 
+    && object2.x-object1.x < object1.width/2 + object2.width/2
+    && object1.y-object2.y < object1.height/2 + object2.height/2 
+    && object2.y-object1.y < object1.height/2 + object2.height/2){
+      return true;
+  }
+  else {
+    return false;
+  }
+}
